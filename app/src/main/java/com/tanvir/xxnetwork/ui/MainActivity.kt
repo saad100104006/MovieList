5ca617a1ba26ab135e6503e5853097a15a8d936f
Md Tanvir Hossain <>
2021-07-21 12:55:05 +0600

    Add MainActivity and res components for themes, colors and string

---
+++ app/src/main/java/com/tanvir/xxnetwork/ui/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.tanvir.xxnetwork.ui
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.tanvir.xxnetwork.R
+import com.tanvir.xxnetwork.databinding.ActivityBottomMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+import me.ibrahimsn.lib.OnItemSelectedListener
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityBottomMainBinding
+    lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityBottomMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        navController = Navigation.findNavController(this@MainActivity, R.id.fragment)
+
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.bottom_nav_menu, menu)
+        val navController = Navigation.findNavController(this, R.id.fragment)
+        binding.smoothBar.setupWithNavController(menu!!, navController)
+        return true
+    }
+}
---
+++ app/src/main/java/com/tanvir/xxnetwork/ui/fragments/HomeFragment.kt
@@ -0,0 +1,13 @@
+package com.tanvir.xxnetwork.ui.fragments
+
+
+import androidx.fragment.app.Fragment
+
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class HomeFragment : Fragment() {
+
+
+}
