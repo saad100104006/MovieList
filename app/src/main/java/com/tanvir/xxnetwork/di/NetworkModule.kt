5455ac5509c1599b703bd0fce0c5f2306232cf99
Md Tanvir Hossain <>
2021-07-22 20:49:25 +0600

    implement cache

--- app/src/main/java/com/tanvir/xxnetwork/di/NetworkModule.kt
+++ app/src/main/java/com/tanvir/xxnetwork/di/NetworkModule.kt
@@ -21,10 +21,42 @@ class NetworkModule {
     @Provides
     @Singleton
     fun provideMovieApiService(): MovieApiService {
+
+        val onlineInterceptor: Interceptor = Interceptor { chain ->
+            val response: Response = chain.proceed(chain.request())
+            val maxAge = 60 // read from cache for 60 seconds even if there is internet connection
+            response.newBuilder()
+                    .header("Cache-Control", "public, max-age=$maxAge")
+                    .removeHeader("Pragma")
+                    .build()
+        }
+
+        val offlineInterceptor = Interceptor { chain ->
+            var request: Request = chain.request()
+            if (!Constants.isNetworkAvailable(BaseApplication.applicationContext())) {
+                val maxStale = 60 * 60 * 24 * 30 // Offline cache available for 30 days
+                request = request.newBuilder()
+                        .header("Cache-Control", "public, only-if-cached, max-stale=$maxStale")
+                        .removeHeader("Pragma")
+                        .build()
+            }
+            chain.proceed(request)
+        }
+        val cacheSize = 10 * 1024 * 1024 // 10 MB
+
+        val cache = Cache(BaseApplication.applicationContext().cacheDir, cacheSize.toLong())
+
+        val okHttpClient = OkHttpClient.Builder()
+                .addInterceptor(offlineInterceptor)
+                .addNetworkInterceptor(onlineInterceptor)
+                .cache(cache)
+                .build()
+
         return Retrofit.Builder()
                 .baseUrl(Constants.BaseURL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+                .client(okHttpClient)
                 .build()
                 .create(MovieApiService::class.java)
     }
